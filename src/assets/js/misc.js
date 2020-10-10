71ec5233c79cb092c251337722eb9c4c9130d552
HiepLP <hiepphuoclly@gmail.com>
2020-10-10 23:27:13 +0700

    remove unused items

--- src/assets/js/misc.js
+++ src/assets/js/misc.js
@@ -1,38 +1,38 @@
 import jQuery from 'jquery'
 
-var ChartColor = [
-  "#5D62B4",
-  "#54C3BE",
-  "#EF726F",
-  "#F9C446",
-  "rgb(93.0, 98.0, 180.0)",
-  "#21B7EC",
-  "#04BCCC",
-];
-var primaryColor = getComputedStyle(document.body).getPropertyValue(
-  "--primary"
-);
-var secondaryColor = getComputedStyle(document.body).getPropertyValue(
-  "--secondary"
-);
-var successColor = getComputedStyle(document.body).getPropertyValue(
-  "--success"
-);
-var warningColor = getComputedStyle(document.body).getPropertyValue(
-  "--warning"
-);
-var dangerColor = getComputedStyle(document.body).getPropertyValue("--danger");
-var infoColor = getComputedStyle(document.body).getPropertyValue("--info");
-var darkColor = getComputedStyle(document.body).getPropertyValue("--dark");
-var lightColor = getComputedStyle(document.body).getPropertyValue("--light");
+// var ChartColor = [
+//   "#5D62B4",
+//   "#54C3BE",
+//   "#EF726F",
+//   "#F9C446",
+//   "rgb(93.0, 98.0, 180.0)",
+//   "#21B7EC",
+//   "#04BCCC",
+// ];
+// var primaryColor = getComputedStyle(document.body).getPropertyValue(
+//   "--primary"
+// );
+// var secondaryColor = getComputedStyle(document.body).getPropertyValue(
+//   "--secondary"
+// );
+// var successColor = getComputedStyle(document.body).getPropertyValue(
+//   "--success"
+// );
+// var warningColor = getComputedStyle(document.body).getPropertyValue(
+//   "--warning"
+// );
+// var dangerColor = getComputedStyle(document.body).getPropertyValue("--danger");
+// var infoColor = getComputedStyle(document.body).getPropertyValue("--info");
+// var darkColor = getComputedStyle(document.body).getPropertyValue("--dark");
+// var lightColor = getComputedStyle(document.body).getPropertyValue("--light");
 
 (function ($) {
   "use strict";
   $(function () {
     var body = $("body");
-    var contentWrapper = $(".content-wrapper");
-    var scroller = $(".container-scroller");
-    var footer = $(".footer");
+    // var contentWrapper = $(".content-wrapper");
+    // var scroller = $(".container-scroller");
+    // var footer = $(".footer");
     var sidebar = $(".sidebar");
 
     //Add active class to nav-link based on url dynamically
@@ -90,7 +90,7 @@ var lightColor = getComputedStyle(document.body).getPropertyValue("--light");
       //Applying perfect scrollbar
       if (!body.hasClass("rtl")) {
         if (body.hasClass("sidebar-fixed")) {
-          var fixedSidebarScroll = new PerfectScrollbar("#sidebar .nav");
+          // var fixedSidebarScroll = new PerfectScrollbar("#sidebar .nav");
         }
       }
     }
